b29445d448f56906fb1b62e7cc14ae4185b59fbd
wilmer <wilmer@localhost>
2021-05-27 17:54:25 -0500

    Merge origin/main into main
    Conflicts:
    	Bankingly_Server/Bankingly-web/src/main/java/com/fenoreste/rest/services/impl/CustomerServices.java

--- webapp-sample3/src/main/java/com/fenoreste/rest/WsTDD/TarjetaDeDebito.java
+++ webapp-sample3/src/main/java/com/fenoreste/rest/WsTDD/TarjetaDeDebito.java
@@ -1,11 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.fenoreste.rest.WsTDD;
 
 import com.fenoreste.rest.Util.AbstractFacade;
+import com.fenoreste.rest.entidades.Tablas;
+import com.fenoreste.rest.entidades.TablasPK;
 import com.fenoreste.rest.entidades.WsFoliosTarjetasSyC1;
 import com.fenoreste.rest.entidades.WsFoliosTarjetasSyCPK1;
 import com.syc.ws.endpoint.siscoop.BalanceQueryResponseDto;
@@ -18,20 +15,71 @@ import javax.persistence.EntityManagerFactory;
  */
 public class TarjetaDeDebito {
     
-    
    
     
     SiscoopTDD SiscoopTdd;
         // CONSULTA Y ACTUALIZA EL SALDO DE LA TarjetaDeDebito
+    
+    
+    
+    private Tablas productoParaTdd(){
+        EntityManagerFactory emf=AbstractFacade.conexion();
+        EntityManager em=emf.createEntityManager();
+        try {
+            TablasPK pkt = new TablasPK("identificador_uso_tdd", "activa");
+            Tablas tb = em.find(Tablas.class, pkt);
+            if(tb!=null){
+                return tb;
+            }else{
+                return null;
+            }
+        } catch (Exception e) {
+            em.close();
+            emf.close();
+            System.out.println("No existe producto activo para tdd:"+e.getMessage());
+        }finally{
+            em.close();
+            emf.close();
+        }
+        return null;
+    }
+    private Tablas parametrosConexion(){
+      EntityManagerFactory emf=AbstractFacade.conexion();
+      EntityManager em=emf.createEntityManager();
+        try {
+            Tablas tbb=productoParaTdd();
+            if(tbb!=null){
+            TablasPK tablasPK = new TablasPK("siscoop_banca_movil", "wsdl");
+            Tablas paramc = em.find(Tablas.class, tablasPK);
+            
+            if(paramc!=null){
+                return paramc;
+            }else{
+                return null;
+            }
+            }
+        } catch (Exception e) {
+            em.close();
+            emf.close();
+            System.out.println("No existen parametros para conexion:"+e.getMessage());
+        }finally{
+            em.close();
+            emf.close();
+        }
+        return null;
+       
+    }
+    
     public BalanceQueryResponseDto saldoTDD(WsFoliosTarjetasSyCPK1 saldotTddPK) {
         EntityManagerFactory emf=AbstractFacade.conexion();
+        EntityManager em=emf.createEntityManager();
+        
         BalanceQueryResponseDto balanceQueryResponseDto = new BalanceQueryResponseDto();
-       
+        WsFoliosTarjetasSyC1 tarjeta =  em.find(WsFoliosTarjetasSyC1.class,saldotTddPK);
+        System.out.println("Llegando al web service de SYC");
         try{
-         EntityManager em=emf.createEntityManager();
-         WsFoliosTarjetasSyC1 tarjeta =  em.find(WsFoliosTarjetasSyC1.class,saldotTddPK);
-         System.out.println("Llegando al web service de SYC");
-        if (tarjeta.getWsSiscoopFoliosTarjetasPK() != null) {
+            Tablas tpws=parametrosConexion();
+        if (tpws!=null) {
             System.out.println("entro al if 1");
             // La tarjeta tiene que estar activa
             if (tarjeta.getActiva()) {
@@ -57,8 +105,8 @@ public class TarjetaDeDebito {
                     }*/
                 } catch (Exception e) {
                     balanceQueryResponseDto.setDescription("Connect timed out");
-                    System.out.println("Error al consultar SYC, tiempo agotado. " + e.getMessage());
                     em.close();
+                    emf.close();
                     
                 }
             } else {
@@ -66,13 +114,20 @@ public class TarjetaDeDebito {
                 //balanceQueryResponseDto.setCode(0);
                 balanceQueryResponseDto.setDescription("La tarjeta esta inactiva: " + tarjeta.getIdtarjeta());
             }
+            em.close();
+            emf.close();
         }
         }catch(Exception e){
+            em.close();
             emf.close();
             System.out.println("Error producido en tdd:"+e.getMessage());
+        }finally{
+        em.clear();
+        em.close();
+        emf.close();
         }
        
-   
+        System.out.println("responseDTO:"+balanceQueryResponseDto);
         return balanceQueryResponseDto;
     }
   
